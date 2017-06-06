b983ef5afe6e6cff94829667ea55b21d9d5def78
Ryan Butler <thebutlah@gmail.com>
2017-06-06 14:05:15 -0400

    Finished describing the graph architecture along with the loss
    + Added the loss and optimizer
    + Added more documentation and comments
    + Added graph initializer
    * Fixed input sizes for x and y

--- model.py
+++ model.py
@@ -23,7 +23,7 @@ class LSTM(object):
 
         Args:
             num_steps:      An integer that is the number of unrolled steps that the LSTM takes. This is not (usually)
-                            the length of the actual sequence
+                            the length of the actual sequence. This number is the maximum
             embedding_size: An integer that is equal to the size of the vectors used to embed the input elements.
                             Example: 10,000 for 10,000 unique words in the vocabulary
             seed:           An integer used to seed the initial random state. Can be None to generate a new random seed.
@@ -38,25 +38,39 @@ class LSTM(object):
 
         self._graph = tf.Graph()
         with self._graph.as_default():
-            batch_size = None
+            tf.set_random_seed(seed)
+            batch_size = None  # Although this variable is never modified, it is present to enhance code readability
 
             with tf.variable_scope('Inputs'):
                 self._x = tf.placeholder(tf.int32, shape=(batch_size, num_steps), name='X')
-                self._hot = tf.one_hot(indices=self._x, depth=embedding_size, name='Hot')
-                self._y = tf.placeholder(tf.int32, shape=(batch_size, embedding_size), name='Y')
-                # self._phase_train = tf.placeholder(tf.bool, 'Phase')
+                self._y = tf.placeholder(tf.int32, shape=(batch_size,), name='Y')
 
-            with tf.variable_scope('Layers') as scope:
-                lstm_cell = tf.contrib.rnn.BasicLSTMCell(num_units=256)
-                initial_state = state = lstm_cell.zero_state(batch_size=tf.shape(self._x)[0], dtype=tf.float32)
+                self._hot = tf.one_hot(indices=self._x, depth=embedding_size, name='Hot')  # X as one-hot encoded
+
+            with tf.variable_scope('Unrolled') as scope:
+                lstm_cell = tf.contrib.rnn.BasicLSTMCell(num_units=256)  # This defines the cell structure
+                state = lstm_cell.zero_state(batch_size=tf.shape(self._x)[0], dtype=tf.float32)  # Initial state
 
                 for i in range(num_steps):
-                    if i > 0: scope.reuse_variables()
-                    output, state = lstm_cell(self._hot[:, i, :], state)
+                    if i > 0: scope.reuse_variables()  # Reuse the variables created in the 1st LSTM cell
+                    output, state = lstm_cell(self._hot[:, i, :], state)  # Step the LSTM through the sequence
 
-                final_output = output
+            with tf.variable_scope('Softmax'):
+                # Parameters
+                w = tf.Variable(tf.random_normal((lstm_cell.output_size, embedding_size), stddev=0.1, name='Weights'))
+                b = tf.Variable(tf.random_normal((embedding_size,), stddev=0.1, name='Bias'))
+                scores = tf.matmul(output, w) + b  # The raw class scores to be fed into the loss function
+                self._y_hat = tf.nn.softmax(scores, name='Y-Hat')  # Class probabilities, (batch_size, embedding_size)
+                self._prediction = tf.argmax(self._y_hat, axis=1, name='Prediction')  # Vector of predicted classes
 
-            self._sess = tf.Session(graph=self._graph)  # Not sure if this really needs to explicitly specify the graph
+            with tf.variable_scope('Pipelining'):
+                self._loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(  # Cross-entropy loss
+                    logits=scores,
+                    labels=self._y
+                ), name='Loss')
+                self._train_step = tf.train.AdamOptimizer(learning_rate=0.01).minimize(self._loss)
+
+            self._sess = tf.Session()
             with self._sess.as_default():
                 self._saver = tf.train.Saver()
                 if load_model is not None:
---
+++ util.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import tensorflow as tf
+import sys
+
+
+def batch_norm(x, shape, axes, phase_train, decay=0.5, scope='BN'):
+    """
+    Batch normalization on tensors.
+    Ref.: http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
+    Note: The original author's code has been modified to generalize the spatial dimensions of the input tensor.
+    Args:
+        x:           Tensor to batch normalize
+        shape:       Tuple, shape of input
+        axes:        Tuple, axes along which mean and variance will be normalized
+        phase_train: Boolean tf.Variable, true indicates training phase
+        decay:       The decay rate for the exponential moving average
+        scope:       String, variable scope
+    Returns:
+        normed:      Batch-normalized tensor
+    """
+    with tf.variable_scope(scope):
+        try:
+            remaining_axes = (shape[axis] for axis in axes)
+        except IndexError:
+            raise ValueError('The given axes are incompatible with the given shape.')
+
+        beta = tf.Variable(tf.constant(0.0, shape=remaining_axes),
+                           name='Beta', trainable=True)
+        gamma = tf.Variable(tf.constant(1.0, shape=remaining_axes),
+                            name='Gamma', trainable=True)
+        batch_mean, batch_var = tf.nn.moments(x, axes=axes, name='Moments')
+        ema = tf.train.ExponentialMovingAverage(decay=decay)
+
+        def mean_var_with_update():
+            ema_apply_op = ema.apply([batch_mean, batch_var])
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+
+        mean, var = tf.cond(phase_train,
+                            mean_var_with_update,
+                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
+        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+    return normed
+
+
+def eprint(*args, **kwargs):
+    """Prints to stderr"""
+    print(*args, file=sys.stderr, **kwargs)
