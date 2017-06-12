3498a495edcc7001fe20dbc367cbc7c566a69161
Ryan Butler <thebutlah@gmail.com>
2017-06-12 15:57:12 -0400

    Made it so that only 1 GPU is viewed and its memory is not hogged

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from __future__ import division
 from __future__ import absolute_import
 
 import numpy as np
+import os
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
 from model import LSTM
 from util import print_examples
-from tensorflow.python.client import timeline
 
 
 def main():
@@ -22,11 +22,9 @@ def main():
 
     np.random.seed(seed)
 
-    config = None
-    import os
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    #config = tf.ConfigProto()
-    #config.gpu_options.allow_growth=True  # Make it so that the program does not grab all GPUs' memory at start
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth=True  # Make it so that the program does not grab all GPUs' memory at start
 
     model = LSTM(embedding_size, backprop_steps, seed=seed, config=config)  # Initialize the model architecture, but do not pass data
 
